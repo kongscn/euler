5a627c316480507291b6782e0bfa414cae91cbf6
Aeol Kong <kongsdev@gmail.com>
2013-08-25 12:47:01 +0800

    update

--- util.py
+++ util.py
@@ -3,7 +3,13 @@ import numpy as np
 def asdigits(s):
     s = ''.join(s.split())
     s = np.array(list(map(int, s)))
+    return s
 
+def asarray(s):
+    lines = s.splitlines()
+    lines = list(map(int, lines))
+    return np.array(lines)
+    
 def as2darray(s):
     lines = s.splitlines()
     lines = [list(map(int, line.split())) for line in lines]
