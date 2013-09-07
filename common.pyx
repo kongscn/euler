f194859d871cf50a38a8eab15a792a2e31ebf227
Aeol Kong <kongsdev@gmail.com>
2013-09-07 15:57:16 +0800

    bug fix

--- common.pyx
+++ common.pyx
@@ -27,6 +27,7 @@ def isPrime(n):
     if n < 2: return False
     if n == 2: return True
     if n == 3: return True
+    if n % 2 == 0: return False
     for i in range(3, int(math.sqrt(n))+1, 2):
         if n % i == 0:
             return False
